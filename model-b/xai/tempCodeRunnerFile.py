0a39f278a811d7a9221494d33673ab73c5c01f40
Joe Morton <joe2.morton@live.uwe.ac.uk>
2026-04-21 15:07:52 +0100

    Reorganise models into correct folders

--- model-b/xai/tempCodeRunnerFile.py
+++
@@ -1,8 +0,0 @@
-print("[XAI] Creating Grad-CAM grid of one healthy + one rotten per fruit...")
-    # make_gradcam_grid()
-
-    # print("[XAI] Creating top error grids...")
-    # make_top_errors_grids()
-
-    # print("[XAI] Creating robustness grids...")
-    # make_robustness_grids()
